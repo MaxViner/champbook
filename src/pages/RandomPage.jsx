60bb72a7c1c0e6e411514d489e31579cfe4d24e6
max <kodtya.oreshin@inbox.ru>
2024-03-26 13:09:00 +0300

    create v-1

--- src/pages/RandomPage.jsx
+++ src/pages/RandomPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { RandomQuote } from '../components/randomQuote/RandomQuote'
 export default function RandomPage() {
   return (
-  
+
       <RandomQuote />
   )
 }
